496b657f6f9660ed6028620426eeb225bd6f8471
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-01-06 17:23:42 +0000

    Function application

--- main.hs
+++ main.hs
@@ -44,12 +44,53 @@ main =
     getArgs >>= print . eval . readExpr . head
 
 
+-- ----
+-- EVAL
+-- ----
+
 eval :: LispVal -> LispVal
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool _) = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
 
+
+apply :: String -> [LispVal] -> LispVal
+apply func args =
+    maybe (Bool False) ($ args) $ lookup func primitives
+
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =
+    [ ("+", numericBinop (+))
+    , ("-", numericBinop (-))
+    , ("*", numericBinop (*))
+    , ("/", numericBinop div)
+    , ("mod", numericBinop mod)
+    , ("quotient", numericBinop quot)
+    , ("remainder", numericBinop rem)
+    ]
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params =
+    Number $ foldl1 op $ map unpackNum params
+
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in 
+                           if null parsed 
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+
+-- ------------------
+-- HIGH LEVEL PARSING
+-- ------------------
 
 readExpr :: String -> LispVal
 readExpr input =
@@ -58,12 +99,9 @@ readExpr input =
         Right val -> val
 
 
--- ------------------
--- HIGH LEVEL PARSING
--- ------------------
-
 parseList :: Parser LispVal
-parseList = liftM List $ sepBy parseExpr spaces
+parseList =
+    liftM List $ sepBy parseExpr spaces
 
 
 parseDottedList :: Parser LispVal
