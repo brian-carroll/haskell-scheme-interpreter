658f0314b60cfca40669790f4498e79afb559aee
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-01-09 16:53:10 +0000

    Refactor weak typing functions into a submodule of Eval

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module Eval (eval, nullEnv) where
 
 -- Libraries
@@ -8,6 +6,7 @@ import Control.Monad.Error
 -- Local modules
 import Types
 import Eval.LispEnv
+import Eval.WeakTyping
 
 
 eval :: Env -> LispVal -> IOThrowsError LispVal
@@ -90,44 +89,6 @@ primitives =
     ]
 
 
-eqv :: [LispVal] -> ThrowsError LispVal
-eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
-eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
-eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
-eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
-eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [(List arg1), (List arg2)]             = listEq eqv arg1 arg2
-eqv [_, _]                                 = return $ Bool False
-eqv badArgList                             = throwError $ NumArgs 2 badArgList
-
-
-listEq :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> [LispVal] -> ThrowsError LispVal
-listEq equalityFn arg1 arg2 =
-    return $ Bool $
-        (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
-    where eqvPair (x1, x2) = case equalityFn [x1, x2] of
-            Left err -> False
-            Right (Bool val) -> val
-
-
-data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
-
-unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
-unpackEquals arg1 arg2 (AnyUnpacker unpacker) = 
-             do unpacked1 <- unpacker arg1
-                unpacked2 <- unpacker arg2
-                return $ unpacked1 == unpacked2
-        `catchError` (const $ return False)
-
-equal :: [LispVal] -> ThrowsError LispVal
-equal [List arg1, List arg2] = listEq equal arg1 arg2
-equal [arg1, arg2] = do
-      primitiveEquals <- liftM or $
-                            mapM (unpackEquals arg1 arg2) 
-                                [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
-      eqvEquals <- eqv [arg1, arg2]
-      return $ Bool $ ((let (Bool x) = eqvEquals in x) || primitiveEquals)
-equal badArgList = throwError $ NumArgs 2 badArgList
 
 
 
@@ -197,26 +158,3 @@ numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError Lisp
 numericBinop op           []  = throwError $ NumArgs 2 []
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params        = mapM unpackNum params >>= return . Number . foldl1 op
-
-
-unpackNum :: LispVal -> ThrowsError Integer
-unpackNum (Number n) = return n
-unpackNum (String n) = let parsed = reads n in
-                           if null parsed
-                             then throwError $ TypeMismatch "number" $ String n
-                             else return $ fst $ parsed !! 0
-unpackNum (List [n]) = unpackNum n
-unpackNum notNum     = throwError $ TypeMismatch "number" notNum
-
-
-unpackStr :: LispVal -> ThrowsError String
-unpackStr (String s) = return s
-unpackStr (Number s) = return $ show s
-unpackStr (Bool s)   = return $ show s
-unpackStr notString  = throwError $ TypeMismatch "string" notString
-
-
-unpackBool :: LispVal -> ThrowsError Bool
-unpackBool (Bool b) = return b
-unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
-
---
+++ src/Eval/WeakTyping.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
+module Eval.WeakTyping
+    ( eqv
+    , equal
+    , unpackBool
+    , unpackNum
+    , unpackStr
+    )
+    where
+
+import Types
+import Control.Monad.Error
+
+
+-- Typeclass to help with weak typing
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+
+-- Strictly typed equality
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]             = listEq eqv arg1 arg2
+eqv [_, _]                                 = return $ Bool False
+eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
+
+-- Weakly typed equality
+equal :: [LispVal] -> ThrowsError LispVal
+equal [List arg1, List arg2] = listEq equal arg1 arg2
+equal [arg1, arg2] = do
+      primitiveEquals <- liftM or $
+                            mapM (unpackEquals arg1 arg2) 
+                                [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+      eqvEquals <- eqv [arg1, arg2]
+      return $ Bool $ ((let (Bool x) = eqvEquals in x) || primitiveEquals)
+equal badArgList = throwError $ NumArgs 2 badArgList
+
+
+
+-- Helper function to recursively test equality of 2 lists
+listEq :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> [LispVal] -> ThrowsError LispVal
+listEq equalityFn arg1 arg2 =
+    return $ Bool $
+        (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+    where eqvPair (x1, x2) = case equalityFn [x1, x2] of
+            Left err -> False
+            Right (Bool val) -> val
+
+
+-- Helper function to test equality under a particular unpacking function
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) = 
+             do unpacked1 <- unpacker arg1
+                unpacked2 <- unpacker arg2
+                return $ unpacked1 == unpacked2
+        `catchError` (const $ return False)
+
+
+
+-- Unpacking functions
+--  Type conversions to implement weak typing
+
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n) = return n
+unpackNum (String n) = let parsed = reads n in
+                           if null parsed
+                             then throwError $ TypeMismatch "number" $ String n
+                             else return $ fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum notNum     = throwError $ TypeMismatch "number" notNum
+
+
+unpackStr :: LispVal -> ThrowsError String
+unpackStr (String s) = return s
+unpackStr (Number s) = return $ show s
+unpackStr (Bool s)   = return $ show s
+unpackStr notString  = throwError $ TypeMismatch "string" notString
+
+
+unpackBool :: LispVal -> ThrowsError Bool
+unpackBool (Bool b) = return b
+unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
