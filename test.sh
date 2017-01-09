d910c136dd43757f62ce9cb0f2f3993a1efa19bc
Brian Carroll <brian.carroll.ireland@gmail.com>
2017-01-09 00:53:56 +0000

    I made a REPL!!! :)

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ghc --make -o eval main.hs
+ghc --make -o lisp main.hs
 
 trap "exit 1" TERM
 export TOP_PID=$$
@@ -17,7 +17,7 @@ function should_be() {
 }
 
 function parser_test {
-    ./eval "(quote $1)" | should_be "$2"
+    ./lisp "(quote $1)" | should_be "$2"
 }
 
 
@@ -61,147 +61,147 @@ expecting space or ")"'
 
 
 # Error for undefined function
-./eval '(undefinedfunction 42)' | should_be 'Unrecognized primitive function: "undefinedfunction"'
+./lisp '(undefinedfunction 42)' | should_be 'Unrecognized primitive function: "undefinedfunction"'
 
 
 # Arithmetic primitive functions
-./eval '(+ 5 2)' | should_be '7'
-./eval '(- 5 2)' | should_be '3'
-./eval '(/ 5 2)' | should_be '2'
-./eval '(* 5 2)' | should_be '10'
-./eval '(mod 5 2)' | should_be '1'
-./eval '(quotient 5 2)' | should_be '2'
-./eval '(remainder 5 2)' | should_be '1'
+./lisp '(+ 5 2)' | should_be '7'
+./lisp '(- 5 2)' | should_be '3'
+./lisp '(/ 5 2)' | should_be '2'
+./lisp '(* 5 2)' | should_be '10'
+./lisp '(mod 5 2)' | should_be '1'
+./lisp '(quotient 5 2)' | should_be '2'
+./lisp '(remainder 5 2)' | should_be '1'
 
 
 # Type testing primitive functions
-./eval '(symbol? abc)' | should_be '#t'
-./eval '(symbol? 42)' | should_be '#f'
+./lisp '(symbol? abc)' | should_be '#t'
+./lisp '(symbol? 42)' | should_be '#f'
 
-./eval '(string? "hello world")' | should_be '#t'
-./eval '(string? 42)' | should_be '#f'
+./lisp '(string? "hello world")' | should_be '#t'
+./lisp '(string? 42)' | should_be '#f'
 
-./eval '(number? 42)' | should_be '#t'
-./eval '(number? #x42)' | should_be '#t'
-./eval '(number? "hello world")' | should_be '#f'
+./lisp '(number? 42)' | should_be '#t'
+./lisp '(number? #x42)' | should_be '#t'
+./lisp '(number? "hello world")' | should_be '#f'
 
-./eval '(char? #\a)' | should_be '#t'
-./eval '(char? #\newline)' | should_be '#t'
-./eval '(char? 42)' | should_be '#f'
+./lisp '(char? #\a)' | should_be '#t'
+./lisp '(char? #\newline)' | should_be '#t'
+./lisp '(char? 42)' | should_be '#f'
 
-./eval '(bool? #t)' | should_be '#t'
-./eval '(bool? #f)' | should_be '#t'
-./eval '(bool? 42)' | should_be '#f'
+./lisp '(bool? #t)' | should_be '#t'
+./lisp '(bool? #f)' | should_be '#t'
+./lisp '(bool? 42)' | should_be '#f'
 
-./eval "(list? '(1 2 3))" | should_be '#t'
-./eval '(list? 42)' | should_be '#f'
+./lisp "(list? '(1 2 3))" | should_be '#t'
+./lisp '(list? 42)' | should_be '#f'
 
-./eval '(symbol?)' | should_be 'Expected 1 args; found values ()'
-./eval '(symbol? 42 42)' | should_be 'Expected 1 args; found values (42 42)'
+./lisp '(symbol?)' | should_be 'Expected 1 args; found values ()'
+./lisp '(symbol? 42 42)' | should_be 'Expected 1 args; found values (42 42)'
 
 
 # symbol handling functions
-./eval '(symbol->string abc)' | should_be '"abc"'
-./eval '(symbol->string 42)' | should_be 'Invalid type: expected Atom, found 42'
-./eval '(symbol->string)' | should_be 'Expected 1 args; found values ()'
-./eval '(symbol->string abc 42)' | should_be 'Expected 1 args; found values (abc 42)'
+./lisp '(symbol->string abc)' | should_be '"abc"'
+./lisp '(symbol->string 42)' | should_be 'Invalid type: expected Atom, found 42'
+./lisp '(symbol->string)' | should_be 'Expected 1 args; found values ()'
+./lisp '(symbol->string abc 42)' | should_be 'Expected 1 args; found values (abc 42)'
 
-./eval '(string->symbol "abc")' | should_be 'abc'
-./eval '(string->symbol 42)' | should_be 'Invalid type: expected String, found 42'
-./eval '(string->symbol)' | should_be 'Expected 1 args; found values ()'
-./eval '(string->symbol abc 42)' | should_be 'Expected 1 args; found values (abc 42)'
+./lisp '(string->symbol "abc")' | should_be 'abc'
+./lisp '(string->symbol 42)' | should_be 'Invalid type: expected String, found 42'
+./lisp '(string->symbol)' | should_be 'Expected 1 args; found values ()'
+./lisp '(string->symbol abc 42)' | should_be 'Expected 1 args; found values (abc 42)'
 
 
 # Comparison operators
-./eval '(= 42 abc)' | should_be 'Invalid type: expected number, found abc'
-./eval '(= 2 abc 123)' | should_be 'Expected 2 args; found values (2 abc 123)'
+./lisp '(= 42 abc)' | should_be 'Invalid type: expected number, found abc'
+./lisp '(= 2 abc 123)' | should_be 'Expected 2 args; found values (2 abc 123)'
 
-./eval '(= 42 42)' | should_be '#t'
-./eval '(= 42 0)' | should_be '#f'
+./lisp '(= 42 42)' | should_be '#t'
+./lisp '(= 42 0)' | should_be '#f'
 
-./eval '(< 2 3)' | should_be '#t'
-./eval '(< 3 2)' | should_be '#f'
+./lisp '(< 2 3)' | should_be '#t'
+./lisp '(< 3 2)' | should_be '#f'
 
-./eval '(> 3 2)' | should_be '#t'
-./eval '(> 2 3)' | should_be '#f'
+./lisp '(> 3 2)' | should_be '#t'
+./lisp '(> 2 3)' | should_be '#f'
 
-./eval '(/= 42 42)' | should_be '#f'
-./eval '(/= 42 0)' | should_be '#t'
+./lisp '(/= 42 42)' | should_be '#f'
+./lisp '(/= 42 0)' | should_be '#t'
 
-./eval '(<= 2 3)' | should_be '#t'
-./eval '(<= 2 2)' | should_be '#t'
-./eval '(<= 3 2)' | should_be '#f'
+./lisp '(<= 2 3)' | should_be '#t'
+./lisp '(<= 2 2)' | should_be '#t'
+./lisp '(<= 3 2)' | should_be '#f'
 
-./eval '(>= 3 2)' | should_be '#t'
-./eval '(>= 3 3)' | should_be '#t'
-./eval '(>= 2 3)' | should_be '#f'
+./lisp '(>= 3 2)' | should_be '#t'
+./lisp '(>= 3 3)' | should_be '#t'
+./lisp '(>= 2 3)' | should_be '#f'
 
-./eval '(&& #t #t)' | should_be '#t'
-./eval '(&& #t #f)' | should_be '#f'
-./eval '(&& #f #t)' | should_be '#f'
-./eval '(&& #f #f)' | should_be '#f'
+./lisp '(&& #t #t)' | should_be '#t'
+./lisp '(&& #t #f)' | should_be '#f'
+./lisp '(&& #f #t)' | should_be '#f'
+./lisp '(&& #f #f)' | should_be '#f'
 
-./eval '(|| #t #t)' | should_be '#t'
-./eval '(|| #t #f)' | should_be '#t'
-./eval '(|| #f #t)' | should_be '#t'
-./eval '(|| #f #f)' | should_be '#f'
+./lisp '(|| #t #t)' | should_be '#t'
+./lisp '(|| #t #f)' | should_be '#t'
+./lisp '(|| #f #t)' | should_be '#t'
+./lisp '(|| #f #f)' | should_be '#f'
 
-./eval '(string=? "hello" "hello")' | should_be '#t'
-./eval '(string=? "hello" "abc")' | should_be '#f'
+./lisp '(string=? "hello" "hello")' | should_be '#t'
+./lisp '(string=? "hello" "abc")' | should_be '#f'
 
-./eval '(string<? "h" "hello")' | should_be '#t'
-./eval '(string<? "hello" "h")' | should_be '#f'
-./eval '(string<? "hello" "hello")' | should_be '#f'
+./lisp '(string<? "h" "hello")' | should_be '#t'
+./lisp '(string<? "hello" "h")' | should_be '#f'
+./lisp '(string<? "hello" "hello")' | should_be '#f'
 
-./eval '(string>? "hello" "h")' | should_be '#t'
-./eval '(string>? "h" "hello")' | should_be '#f'
-./eval '(string>? "hello" "hello")' | should_be '#f'
+./lisp '(string>? "hello" "h")' | should_be '#t'
+./lisp '(string>? "h" "hello")' | should_be '#f'
+./lisp '(string>? "hello" "hello")' | should_be '#f'
 
-./eval '(string<=? "h" "hello")' | should_be '#t'
-./eval '(string<=? "hello" "h")' | should_be '#f'
-./eval '(string<=? "hello" "hello")' | should_be '#t'
+./lisp '(string<=? "h" "hello")' | should_be '#t'
+./lisp '(string<=? "hello" "h")' | should_be '#f'
+./lisp '(string<=? "hello" "hello")' | should_be '#t'
 
-./eval '(string>=? "hello" "h")' | should_be '#t'
-./eval '(string>=? "h" "hello")' | should_be '#f'
-./eval '(string>=? "hello" "hello")' | should_be '#t'
+./lisp '(string>=? "hello" "h")' | should_be '#t'
+./lisp '(string>=? "h" "hello")' | should_be '#f'
+./lisp '(string>=? "hello" "hello")' | should_be '#t'
 
-./eval '(if (> 2 3) "yes" "no")' | should_be '"no"'
-./eval '(if (< 2 3) "yes" "no")' | should_be '"yes"'
-./eval '(if 1 2 3 4)' | should_be 'Expected 3 args; found values (1 2 3 4)'
-./eval '(if 1 2)' | should_be 'Expected 3 args; found values (1 2)'
+./lisp '(if (> 2 3) "yes" "no")' | should_be '"no"'
+./lisp '(if (< 2 3) "yes" "no")' | should_be '"yes"'
+./lisp '(if 1 2 3 4)' | should_be 'Expected 3 args; found values (1 2 3 4)'
+./lisp '(if 1 2)' | should_be 'Expected 3 args; found values (1 2)'
 
-./eval "(car '(1 2 3))" | should_be '1'
-./eval "(car '(1))" | should_be '1'
-./eval "(car '(1 2 . 3))" | should_be '1'
-./eval '(car "thing")' | should_be 'Invalid type: expected pair, found "thing"'
-./eval '(car 1 2 3)' | should_be 'Expected 1 args; found values (1 2 3)'
+./lisp "(car '(1 2 3))" | should_be '1'
+./lisp "(car '(1))" | should_be '1'
+./lisp "(car '(1 2 . 3))" | should_be '1'
+./lisp '(car "thing")' | should_be 'Invalid type: expected pair, found "thing"'
+./lisp '(car 1 2 3)' | should_be 'Expected 1 args; found values (1 2 3)'
 
-./eval "(cdr '(a b c))" | should_be '(b c)'
-./eval "(cdr '(a b))" | should_be '(b)'
-./eval "(cdr '(a))" | should_be '()'
-./eval "(cdr '(a . b))" | should_be 'b'
-./eval "(cdr '(a b . c))" | should_be '(b . c)'
-./eval "(cdr 'a)" | should_be 'Invalid type: expected pair, found a'
-./eval "(cdr 'a 'b)" | should_be 'Expected 1 args; found values (a b)'
+./lisp "(cdr '(a b c))" | should_be '(b c)'
+./lisp "(cdr '(a b))" | should_be '(b)'
+./lisp "(cdr '(a))" | should_be '()'
+./lisp "(cdr '(a . b))" | should_be 'b'
+./lisp "(cdr '(a b . c))" | should_be '(b . c)'
+./lisp "(cdr 'a)" | should_be 'Invalid type: expected pair, found a'
+./lisp "(cdr 'a 'b)" | should_be 'Expected 1 args; found values (a b)'
 
-./eval "(cons 1 ())" | should_be '(1)'
-./eval "(cons 1 '(2 3))" | should_be '(1 2 3)'
-./eval "(cons 1 '(2 . 3))" | should_be '(1 2 . 3)'
-./eval "(cons 1 2)" | should_be '(1 . 2)'
-./eval "(cons '(1 2) 3)" | should_be '((1 2) . 3)'
-./eval "(cons 1 2 3)" | should_be 'Expected 2 args; found values (1 2 3)'
-./eval "(cons 1)" | should_be 'Expected 2 args; found values (1)'
+./lisp "(cons 1 ())" | should_be '(1)'
+./lisp "(cons 1 '(2 3))" | should_be '(1 2 3)'
+./lisp "(cons 1 '(2 . 3))" | should_be '(1 2 . 3)'
+./lisp "(cons 1 2)" | should_be '(1 . 2)'
+./lisp "(cons '(1 2) 3)" | should_be '((1 2) . 3)'
+./lisp "(cons 1 2 3)" | should_be 'Expected 2 args; found values (1 2 3)'
+./lisp "(cons 1)" | should_be 'Expected 2 args; found values (1)'
 
-./eval "(eqv? '(1 2 3) '(1 2 3))" | should_be '#t'
-./eval '(eqv? 2 2)' | should_be '#t'
-./eval '(eqv? 2 "2")' | should_be '#f'
+./lisp "(eqv? '(1 2 3) '(1 2 3))" | should_be '#t'
+./lisp '(eqv? 2 2)' | should_be '#t'
+./lisp '(eqv? 2 "2")' | should_be '#f'
 
-./eval "(equal? '(1 2 3) '(1 2 3))" | should_be '#t'
-./eval '(equal? 2 2)' | should_be '#t'
-./eval '(equal? 2 "2")' | should_be '#t'
-./eval '(equal? (quote 2) "2")' | should_be '#t'
-./eval '(equal? (quote (1 "2")) (quote (1 2)))' | should_be '#t' # recursive weak typing
-./eval '(equal? 2 3)' | should_be '#f'
+./lisp "(equal? '(1 2 3) '(1 2 3))" | should_be '#t'
+./lisp '(equal? 2 2)' | should_be '#t'
+./lisp '(equal? 2 "2")' | should_be '#t'
+./lisp '(equal? (quote 2) "2")' | should_be '#t'
+./lisp '(equal? (quote (1 "2")) (quote (1 2)))' | should_be '#t' # recursive weak typing
+./lisp '(equal? 2 3)' | should_be '#f'
 
 
 # If we haven't exited yet then all tests must have passed
